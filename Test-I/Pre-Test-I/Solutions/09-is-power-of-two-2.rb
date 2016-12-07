ca9b2060e6c4ef9faaa3b5e0c6be2f11f1fc27db
Alexander Shekhtman <alexander@shekhtman.com>
2016-12-07 15:35:10 -0800

    Add files via upload

---
+++ Test-I/Pre-Test-I/Solutions/09-is-power-of-two-2.rb
@@ -0,0 +1,27 @@
+# Write a method that takes in a number and returns true if it is a
+# power of 2. Otherwise, return false.
+#
+# You may want to use the `%` modulo operation. `5 % 2` returns the
+# remainder when dividing 5 by 2; therefore, `5 % 2 == 1`. In the case
+# of `6 % 2`, since 2 evenly divides 6 with no remainder, `6 % 2 == 0`.
+#
+# Difficulty: medium.
+
+def is_power_of_two?(number)
+ return false if number == 0
+ while(number % 2 == 0)
+   number = number / 2
+ end
+ return false if number > 1
+ true
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('is_power_of_two?(1) == true: ' + (is_power_of_two?(1) == true).to_s)
+puts('is_power_of_two?(16) == true: ' + (is_power_of_two?(16) == true).to_s)
+puts('is_power_of_two?(64) == true: ' + (is_power_of_two?(64) == true).to_s)
+puts('is_power_of_two?(78) == false: ' + (is_power_of_two?(78) == false).to_s)
+puts('is_power_of_two?(0) == false: ' + (is_power_of_two?(0) == false).to_s)
+puts('is_power_of_two?(25) ==  true: ' + (is_power_of_two?(25) == true).to_s)
---
+++ Test-I/Pre-Test-I/Solutions/15-is-prime.rb
@@ -0,0 +1,45 @@
+# Write a method that takes in an integer (greater than one) and
+# returns true if it is prime; otherwise return false.
+#
+# You may want to use the `%` modulo operation. `5 % 2` returns the
+# remainder when dividing 5 by 2; therefore, `5 % 2 == 1`. In the case
+# of `6 % 2`, since 2 evenly divides 6 with no remainder, `6 % 2 == 0`.
+# More generally, if `m` and `n` are integers, `m % n == 0` if and only
+# if `n` divides `m` evenly.
+#
+# You would not be expected to already know about modulo for the
+# challenge.
+#
+# Difficulty: medium.
+
+def is_prime?(number)
+  if number <= 1 || number == 4
+    # only numbers > 1 can be prime.
+    return false
+  end
+
+  idx = 3
+  while (idx <= Math.sqrt(number))
+    
+    if (number % idx) == 0
+      return false
+    end
+
+    idx += 2
+  end
+
+  return true
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts('is_prime?(2) == true: ' + (is_prime?(2) == true).to_s)
+puts('is_prime?(3) == true: ' + (is_prime?(3) == true).to_s)
+puts('is_prime?(4) == false: ' + (is_prime?(4) == false).to_s)
+puts('is_prime?(9) == false: ' + (is_prime?(9) == false).to_s)
+puts('is_prime?(13) == true: ' + (is_prime?(13) == true).to_s)
+puts('is_prime?(17) == true: ' + (is_prime?(17) == true).to_s)
+puts('is_prime?(19) == true: ' + (is_prime?(19) == true).to_s)
+puts('is_prime?(23) == true: ' + (is_prime?(23) == true).to_s)
+puts('is_prime?(7919) == true: ' + (is_prime?(7919) == true).to_s)
